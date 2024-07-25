ccf660bd3e69a6725f9ed62136fa99905f5011ab
Slinck0 <nickstorm2005@gmail.com>
2024-07-26 01:57:45 +0200

    swablu
    (:

--- Assets/Scripts/Pokemon/Types/Swablu.cs
+++ Assets/Scripts/Pokemon/Types/Swablu.cs
@@ -16,13 +16,12 @@ public class Swablu : Piece
         Steps = Speed;
         Sprite = Resources.Load<Sprite>(FilePaths.Swablu);
         Scale = 1.3f;
-        Abilities[0] = new Roost(this);
 
         Events.OnTakeDamageStart += (object? sender, Attack Attack) =>
         {
             Debug.Log($"Taking {Attack.Damage} damage");
-            // Attack.Damage = 0;
-            // Debug.Log($"But swablu is invulnerable so attack damage was reduced to {Attack.Damage}");
+            Attack.Damage = 0;
+            Debug.Log($"But swablu is invulnerable so attack damage was reduced to {Attack.Damage}");
         };
 
     }
@@ -30,26 +29,5 @@ public class Swablu : Piece
     public override string GetContents()
     {
         return "swablu";
-    }
-}
-
-public class Roost : ActiveAbility
-{
-    private Swablu swablu;
-
-    public Roost(Swablu swablu)
-    {
-        this.swablu = swablu;
-    }
-
-    public override void OnUse()
-    {
-        int healAmount = swablu.MaxHP / 2;
-        swablu.HP += healAmount;
-        if (swablu.HP > swablu.MaxHP)
-        {
-            swablu.HP = swablu.MaxHP;
-        }
-        Debug.Log($"Swablu used Roost and healed for {healAmount} HP. Current HP: {swablu.HP}");
     }
 }
